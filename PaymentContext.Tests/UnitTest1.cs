b84e55e4755c2ec2624ba2e639bdc4b9afde0c26
Nakati <teonakati@hotmail.com>
2021-08-27 00:08:00 -0400

    Encapsulando as classes

--- PaymentContext.Tests/UnitTest1.cs
+++ PaymentContext.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using PaymentContext.Domain.Entities;
 using Xunit;
 
 namespace PaymentContext.Tests
@@ -6,9 +7,11 @@ namespace PaymentContext.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void AddSubscription()
         {
-
+            var subscription = new Subscription(DateTime.Today.AddDays(30));
+            var student = new Student("Teo", "Nakati", "9398217893", "hello@nakati.com");
+            student.AddSubscription(subscription);
         }
     }
 }
